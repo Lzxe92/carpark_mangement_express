4ceadad8076bba04ede24044eadcfc0620684b30
Eric <contact@eric.sg>
2020-06-10 07:04:04 +0800

    refactored mall.service.js to remove redundant try catch

--- services/mall.service.js
+++ services/mall.service.js
@@ -1,22 +1,16 @@
 var models = require('../models');
 exports.createMall = async function (data) {
-    try {
-        const mall = await models.Mall.create({
-            name: data.name,
-            address: data.address,
-            postal_code: data.postal_code,
-        }).then(function (mall) {
-            if (mall) {
-                return mall
-            }
-        }).catch((error) => {
-            console.log("debug", error);
-            throw Error(error);
-        });
-        return mall;
-    } catch (error) {
-        // Log Errors
+    const mall = await models.Mall.create({
+        name: data.name,
+        address: data.address,
+        postal_code: data.postal_code,
+    }).then(function (mall) {
+        if (mall) {
+            return mall
+        }
+    }).catch((error) => {
         console.log("debug", error);
         throw Error(error);
-    }
+    });
+    return mall;
 }
