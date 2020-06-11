7bf221d18ba9d0d8f1783da99585486631c41425
Eric <contact@eric.sg>
2020-06-11 20:49:26 +0800

    refactored car model

---
+++ migrations/20200611123425-create-car.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('car', {
+      car_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      carplate_number: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('car');
+  }
+};
