48c48e013a2bc8537199b2599ef7d7a4498b018f
Eric <contact@eric.sg>
2020-06-11 19:39:48 +0800

    added unit test for finding all malls

--- tests/services/mall.service.test.js
+++ tests/services/mall.service.test.js
@@ -14,31 +14,57 @@ describe("MallService", function () {
             truncate: {cascade: true}
         });
     });
-    const stubValue = {
+    const stubValue = [{
         name: faker.random.uuid(),
         postal_code: faker.address.zipCode("######"),
         address: faker.address.streetAddress("###"),
         createdAt: faker.date.past(),
         updatedAt: faker.date.past()
-    };
+    }, {
+        name: faker.random.uuid(),
+        postal_code: faker.address.zipCode("######"),
+        address: faker.address.streetAddress("###"),
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.past()
+    }];
     describe("Create a new mall with dummy data", function () {
         it("It should return a mall object with the same value as dummy data", async function () {
             const mallService = new MallService();
-            const result = await mallService.createMall(stubValue);
-            assert.isNotNull(result, 'Creation of mall failed');
-            expect(result.name).to.equal(stubValue.name);
-            expect(result.postal_code).to.equal(stubValue.postal_code);
-            expect(result.address).to.equal(stubValue.address);
+            for (i = 0; i < stubValue.length; i++) {
+                const result = await mallService.createMall(stubValue[i]);
+                assert.isNotNull(result, 'Creation of mall failed');
+                expect(result.name).to.equal(stubValue[i].name);
+                expect(result.postal_code).to.equal(stubValue[i].postal_code);
+                expect(result.address).to.equal(stubValue[i].address);
+            }
         });
     });
-    describe("Find mall with created dummy data", function () {
+    describe("Find a mall with created dummy data", function () {
         it("It should return a mall object with the same value as dummy data", async function () {
             const mallService = new MallService();
-            var result = await mallService.createMall(stubValue);
-            result = await mallService.findOne({mall_id: result.mall_id});
-            expect(result.name).to.equal(stubValue.name);
-            expect(result.postal_code.toString()).to.equal(stubValue.postal_code);
-            expect(result.address).to.equal(stubValue.address);
+            for (i = 0; i < stubValue.length; i++) {
+                var result = await mallService.createMall(stubValue[i]);
+                result = await mallService.findOne({mall_id: result.mall_id});
+                expect(result.name).to.equal(stubValue[i].name);
+                expect(result.postal_code.toString()).to.equal(stubValue[i].postal_code);
+                expect(result.address).to.equal(stubValue[i].address);
+            }
+        });
+    });
+    describe("Find malls with created dummy data", function () {
+        it("It should return a mall object with the same value as dummy data", async function () {
+            const mallService = new MallService();
+            for (i = 0; i < stubValue.length; i++) {
+                var result = await mallService.createMall(stubValue[i]);
+            }
+            result = await mallService.findAll(1,10);
+            for (i = 0; i < result.length; i++) {
+                expect(result[i].name).to.equal(stubValue[i].name);
+                expect(result[i].postal_code.toString()).to.equal(stubValue[i].postal_code);
+                expect(result[i].address).to.equal(stubValue[i].address);
+            }
+
+
         });
     });
 });
